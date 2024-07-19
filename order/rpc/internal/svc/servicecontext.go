a498d6c2fbafae28ad666c4864c5797c82015e16
林庭楷 <hellolinpeter@gmail.com>
2024-07-19 13:10:13 +0800

    order

--- order/rpc/internal/svc/servicecontext.go
+++ order/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,27 @@
 package svc
 
-import "go-mirco-zero/order/rpc/internal/config"
+import (
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	"github.com/zeromicro/go-zero/zrpc"
+	"go-mirco-zero/order/model"
+	"go-mirco-zero/order/rpc/internal/config"
+	"go-mirco-zero/product/rpc/productclient"
+	"go-mirco-zero/user/rpc/userclient"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config     config.Config
+	OrderModel model.OrderModel
+	UserRpc    userclient.User
+	ProductRpc productclient.Product
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	conn := sqlx.NewMysql(c.Mysql.DataSource)
 	return &ServiceContext{
-		Config: c,
+		Config:     c,
+		OrderModel: model.NewOrderModel(conn, c.CacheRedis),
+		UserRpc:    userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
+		ProductRpc: productclient.NewProduct(zrpc.MustNewClient(c.ProductRpc)),
 	}
 }
