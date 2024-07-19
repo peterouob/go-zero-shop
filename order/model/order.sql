a7be0be7723306eadd805b512a11c1aae8954b66
林庭楷 <hellolinpeter@gmail.com>
2024-07-19 22:47:18 +0800

    fix port

--- order/model/order.sql
+++ order/model/order.sql
@@ -1,11 +1,12 @@
-CREATE TABLE `product` (
+CREATE TABLE `order` (
 	`id` bigint unsigned NOT NULL AUTO_INCREMENT,
-	`name` varchar(255)  NOT NULL DEFAULT '' COMMENT '产品名称',
-	`desc` varchar(255)  NOT NULL DEFAULT '' COMMENT '产品描述',
-        `stock` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '产品库存',
-	`amount` int(10) unsigned NOT NULL DEFAULT '0'  COMMENT '产品金额',
-	`status` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '产品状态',
+	`uid` bigint unsigned NOT NULL DEFAULT '0' COMMENT '用户ID',
+	`pid` bigint unsigned NOT NULL DEFAULT '0' COMMENT '产品ID',
+	`amount` int(10) unsigned NOT NULL DEFAULT '0'  COMMENT '订单金额',
+	`status` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '订单状态',
 	`create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
 	`update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY (`id`)
+	PRIMARY KEY (`id`),
+	KEY `idx_uid` (`uid`),
+	KEY `idx_pid` (`pid`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
